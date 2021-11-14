1f9edc1338bb603a69d44657863d9d4c76656349
Germán Insua <1996GInsua@gmail.com>
2021-11-14 18:43:52 +0100

    LAB_SESSION_3 Unity Project Finished

--- LAB_SESSION_3/Assets/Scripts/Client.cs
+++ LAB_SESSION_3/Assets/Scripts/Client.cs
@@ -1,62 +1,129 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.UI;
+
+using System.Net.Sockets;
+using System.IO;
+using System;
 
 public class Client : MonoBehaviour
 {
+    public GameObject chatContainer;
+    public GameObject msgPrefab;
 
-    TCP clientTcp;
+    private bool clientConnected;
+    private TcpClient handler;
+    private NetworkStream stream;
+    private StreamWriter writer;
+    private StreamReader reader;
 
-    public Text chatLog;
-    public InputField inputChat;
-    public Button sendButton;
-
-    List<string> chatHistory;
-    private string display = "";
-
-    void Start()
+    public void Connect()
     {
-        //Initialize server and chatlog to receive incoming msgs
-        clientTcp = new TCP(TCP.tcpType.CLIENT, "127.0.0.1", 6000);
-
-        chatHistory = new List<string>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Reload chat canvas
-        chatLog.text = clientTcp.ReceiveClientMsg();
-    }
-
-    private void OnDestroy()
-    {
-        clientTcp.Shutdown();
-    }
-
-    public void Send()
-    {
-        clientTcp.Send(inputChat.text);
-        Debug.Log("Sending message to Client");
-    }
-
-    public bool MsgPool()
-    {
-        bool doOnce = true;
-
-        if (doOnce)
+        //Avoid next code if our client currently connected
+        if (clientConnected)
         {
-            foreach (string msg in chatHistory)
-            {
-                display = display.ToString() + msg.ToString() + "\n";
-            }
-            chatLog.text = display;
-            doOnce = !doOnce;
+            Debug.Log("Current session already connected!");
+            return;
         }
 
-        return doOnce;
+        try
+        {
+            handler = new TcpClient("127.0.0.1", 6000);
+            stream = handler.GetStream();
+            writer = new StreamWriter(stream);
+            reader = new StreamReader(stream);
+            clientConnected = true;
+        }
+        catch(Exception e)
+        {
+            Debug.LogWarning(e.ToString());
+        }
+
+        //Disable button after connecting
+        Destroy(GameObject.Find("ConnectButton"));
     }
 
+    //Receive message if data available, and send if key pressed
+    private void Update()
+    {
+        if (clientConnected)
+        {
+            if (stream.DataAvailable)
+            {
+                string data = reader.ReadLine();
+                if (data != null)
+                    OnIncomingData(data);
+            }
+        }
+
+       
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                OnSend();
+            }
+        
+    }
+
+    //Create message prefab into our scrollview each time a streamdata received
+    private void OnIncomingData(string data)
+    {
+
+        GameObject go = Instantiate(msgPrefab, chatContainer.transform) as GameObject;
+        Component[] textChilds = go.GetComponentsInChildren<Text>();
+
+        foreach (Text child in textChilds)
+        {
+            if (child.name == "MessageText")
+                child.text = data;
+
+            if (child.name == "MessageHour")
+                child.text = DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString();
+        }
+    }
+
+
+    public void Send(string data)
+    {
+        if (!clientConnected)
+            return;
+
+        writer.WriteLine(data);
+        writer.Flush();
+    }
+
+    public void OnSend()
+    {
+
+        string message = GameObject.Find("InputField").GetComponent<InputField>().text;
+        Send(message);
+
+        GameObject.Find("InputField").GetComponent<InputField>().text = ""; 
+    }
+
+    //Disconnect socket and delete stream writer and reader
+    public void ShutDown()
+    {
+
+        //Check if our client still connected
+        if (!clientConnected)
+            return;
+
+        //Close new statements done in Connect Process
+        writer.Close();
+        reader.Close();
+        handler.Close();
+
+        clientConnected = false;
+    }
+
+    private void OnApplicationQuit()
+    {
+        ShutDown();
+    }
+
+    private void OnDisable()
+    {
+        ShutDown();
+    }
 }
--- LAB_SESSION_3/Assets/Scripts/Server.cs
+++ LAB_SESSION_3/Assets/Scripts/Server.cs
@@ -1,62 +1,313 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using UnityEngine.UI;
+
+using System;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
 
 public class Server : MonoBehaviour
 {
+    private List<Player> clients;
+    private List<Player> disconnectedClients;
+    private List<string> commands;
 
-    TCP serverTcp;
 
-    public Text chatLog;
-    public InputField inputChat;
-    public Button sendButton;
+    private int port = 6000;
+    public TcpListener listener;
+    private bool initialized;
 
-    List<string> chatHistory;
-    private string display = "";
-
-    void Start()
+    public enum COMMAND
     {
-        //Initialize server and chatlog to receive incoming msgs
-        serverTcp = new TCP(TCP.tcpType.SERVER, "127.0.0.1", 6000);
-        
-        chatHistory = new List<string>();
+        ERROR = 0,
+        HELP,
+        LIST,
+        WHISPER,
+        NAME,
+        MAX_COMMANDS
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        //Reload chat canvas
-        chatLog.text = serverTcp.ReceiveServerMsg();
-    }
+        clients = new List<Player>();
+        disconnectedClients = new List<Player>();
+        commands = new List<string>();
 
-    private void OnDestroy()
-    {
-        serverTcp.Shutdown();
-    }
 
-    public void Send()
-    {
-        serverTcp.Send(inputChat.text);
-        Debug.Log("Sending message to Client");
-    }
-
-    public bool MsgPool()
-    {
-        bool doOnce = true;
-
-        if (doOnce)
+        //Initialize server
+        try
         {
-            foreach (string msg in chatHistory)
-            {
-                display = display.ToString() + msg.ToString() + "\n";
-            }
-            chatLog.text = display;
-            doOnce = !doOnce;
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+
+            StartListening();
+            initialized = true;
+
+            Debug.Log("Server started on port: " + port.ToString());
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning(e.ToString());
         }
 
-        return doOnce;
+        commands.Add("/help");
+        commands.Add("/list");    
+        commands.Add("/whisper");
+        commands.Add("/changeName");
     }
 
+    private void Update()
+    {
+        if (!initialized)
+            return;
+
+        //Check non-null data for each client connected
+        foreach (Player c in clients)
+        {
+            //Verify is we have still players connected
+            if (!IsConnected(c.client))
+            {
+                c.client.Close();
+                disconnectedClients.Add(c);
+                continue;
+            }
+            else
+            {
+                NetworkStream s = c.client.GetStream();
+                if (s.DataAvailable)
+                {
+                    StreamReader reader = new StreamReader(s, true);
+                    string data = reader.ReadLine();
+
+                    if (data != null)
+                    {
+                        OnIncomingData(c, data);
+                    }
+                }
+            }
+        }
+
+        //Detect and broadcast disconnecting message for each client in the list
+        for (int i = 0; i < disconnectedClients.Count - 1; i++)
+        {
+            BroadCast(disconnectedClients[i].clientName + "<color=red> disconnected</color>", clients); 
+
+            clients.Remove(disconnectedClients[i]);
+            disconnectedClients.RemoveAt(i);
+        }
+    }
+
+    public void StartListening()
+    {
+        listener.BeginAcceptTcpClient(AcceptClient, listener);
+    }
+
+    //Receive incoming connection and register user into our connected list
+    public void AcceptClient(IAsyncResult ias)
+    {
+        TcpListener listener = ias.AsyncState as TcpListener;
+        Player client = new Player(listener.EndAcceptTcpClient(ias));
+        clients.Add(client);
+        
+        StartListening();
+
+        BroadCast(clients[clients.Count - 1].clientName + " <color=green>has connected</color>", clients);
+        BroadCastClient("Welcome! Use <i>/help</i> command for more information and use <i>/changeName</i> to set your username", client);
+    }
+
+    //Check if any client its still connected sending a poll to the client socket
+    public bool IsConnected(TcpClient c)
+    {
+        try
+        {
+            if (c != null && c.Client != null && c.Client.Connected)
+            {
+                if (c.Client.Poll(0, SelectMode.SelectRead))
+                    return !(c.Client.Receive(new byte[1], SocketFlags.Peek) == 0);
+
+                return true;
+            }
+            else
+                return false;
+
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    //When data received operate, by default broadcast to all, if we find any command then alternate between options
+    public void OnIncomingData(Player client, string data)
+    {
+        //Gets data and stores just the command /xxxx;
+        string commandOrder = SplitCommand(data)[0];
+
+        //String to operate depending case
+        string dataStored = string.Empty;
+
+        //Make sure ur doing a command petition and dont broadcast this info to other clients
+        if (data.StartsWith("/"))
+        {
+
+            switch (CommandString(commandOrder))
+            {
+                case COMMAND.HELP:
+
+                    //Stores all commands into an string and just broadcast to client asking for
+                    foreach (string command in commands)
+                        dataStored += " " + command;
+                    BroadCastClient(dataStored, client);
+                    break;
+
+                case COMMAND.LIST:
+
+                    foreach (Player c in clients)
+                        dataStored += " " + c.clientName;
+                    BroadCastClient(dataStored, client);
+                    break;
+
+                case COMMAND.WHISPER:
+                    //Create temporal list between source and destination and broadcast that message through the created list
+                    List<Player> whisperList = new List<Player>();
+                    whisperList.Add(client);
+                    whisperList.Add(SearchUserConnected(data,clients));
+                    BroadCast(data, whisperList);
+                    break;
+
+                case COMMAND.NAME:
+                    //Get name separated by space in command prompt and checks if any connected user already using Ex. /changeName H3Dio
+                    dataStored = SplitCommand(data)[1];
+
+                    if (CheckUserNames(dataStored))
+                    {
+                        client.SetName(SplitCommand(data)[1]);
+                        BroadCastClient("Your name has been updated to: " + client.clientName, client);
+                    }
+                    else
+                    {
+                        BroadCastClient("Your name it's already being <color=red>used by other user</color> in server. Try again", client);
+                    }
+                    break;
+            }            
+            return;
+        }
+
+        BroadCast("<i>" + client.clientName + "</i> sent: " + data, clients);
+    }
+
+    //Server shares message with current users connected
+    public void BroadCast(string data, List<Player> clients)
+    {
+        foreach(Player c in clients)
+        {
+            try
+            {
+                StreamWriter write = new StreamWriter(c.client.GetStream());
+                write.WriteLine(data);
+                write.Flush();
+            }
+            catch(Exception e)
+            {
+                Debug.LogWarning(e.ToString());
+            }
+        }
+    }
+
+    //Server shares message only to client asking for something
+    public void BroadCastClient(string data, Player client)
+    {
+        try
+        {
+            StreamWriter write = new StreamWriter(client.client.GetStream());
+            write.WriteLine(data);
+            write.Flush();
+        } 
+        catch(Exception e)
+        {
+            Debug.LogWarning(e.ToString());
+        }
+    }
+
+    //Returns user searched by name
+    public Player SearchUserConnected(string data, List<Player> clients)
+    {
+        Player destination = null;
+
+        //Separate string data split "/whisper Client2" to /whisper + Client2;
+        foreach(Player dest in clients)
+        {
+            //If you find a coincidence between names send a message just for the connected client
+            if (SplitCommand(data)[1] == dest.clientName)
+            {
+                Debug.Log(SplitCommand(data)[1]);
+                return dest;
+            }
+        }
+ 
+        return destination;
+    }
+
+    //Returns proper command depending our data input if we start data with "/" char
+    public COMMAND CommandString(string data)
+    {
+        COMMAND order = COMMAND.ERROR;
+
+        switch (data)
+        {
+            case "/help":
+                order = COMMAND.HELP;
+                break;
+
+            case "/list":
+                order = COMMAND.LIST;
+                break;
+
+            case "/whisper":
+                order = COMMAND.WHISPER;
+                break;
+
+            case "/changeName":
+                order = COMMAND.NAME;
+                break;
+        }
+        return order;
+    }
+
+    //Return false if picked name already exist in our server
+    public bool CheckUserNames(string name)
+    {
+        foreach (Player client in clients)
+            if (name == client.clientName)
+                return false;
+
+        return true;
+    }
+
+    //Break string command into 2 or more string separated between spaces into an array to access Colours, Names, Clients...
+    public string[] SplitCommand(string data)
+    {
+        return data.Split(' ');
+    }
+
+}
+
+//Own class to store our client name
+public class Player
+{
+    public TcpClient client;
+    public Guid playerUUID;
+    public string clientName;
+    
+    public Player(TcpClient socket)
+    {
+        playerUUID = Guid.NewGuid();
+        clientName = "Guest-" + playerUUID.ToString();
+        client = socket;
+    }
+
+    public Guid GetUUID() { return playerUUID; }
+    public void SetName(string name) { clientName = name; }
 }
