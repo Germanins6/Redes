c95fceb04cd6add498f716a0ae1c89a7184b4d82
Germán Insua <1996GInsua@gmail.com>
2021-12-26 20:23:40 +0000

    Discard this commit

--- pong_multiplayer/Assets/Scripts/Network/NetworkingServer.cs
+++ pong_multiplayer/Assets/Scripts/Network/NetworkingServer.cs
@@ -71,14 +71,53 @@ public class NetworkingServer : Networking
     }
 
    
-    private void BroadcastWorldState()
+    private void HandShakePlayer(Client tmp_Client)
+    {
+        string id = GenerateUUID();
+
+        tmp_Client.id = id;
+        tmp_Client.socket_client = remote;
+
+        //Assign paddles to each player
+        if (!paddle)
+        {
+            tmp_Client.PaddleInUse = 1.ToString();
+            paddle = !paddle;
+        }
+        else
+        {
+            tmp_Client.PaddleInUse = 2.ToString();
+        }
+
+        clients.Add(id, tmp_Client);
+
+        tmp_Client.PackageType = 1.ToString();
+
+        packageDataSnd = new byte[1024];
+        SerializeClient(tmp_Client);
+        msgToshow = tmp_Client.id.ToString();
+        SendMsg(tmp_Client);
+    }
+
+    private void BroadcastWorldState(Client tmp_Client)
     {
         //Foreach pair of player stored in our clientList send worldState
-        foreach(KeyValuePair<string, Client> player in clients)
+        MovePaddles(tmp_Client);
+        Debug.LogError(world_Replication.Paddle1Pos + "   " + world_Replication.Paddle2Pos);
+
+        world_Replication.PackageType = 4.ToString();
+
+        packageDataSnd = new byte[1024];
+        SerializeData(world_Replication);
+
+
+        foreach (KeyValuePair<string, Client> player in clients)
         {
-            //Call Replication World function each 100ms(?).
-            //WordReplication(player.Value.ep, ¿WorldStateData[]?);
+            if (tmp_Client.id == player.Key)
+                tmp_Client.socket_client = player.Value.socket_client;
         }
+
+        SendMsg(tmp_Client);
     }
 
     // Update is called once per frame
@@ -107,90 +146,33 @@ public class NetworkingServer : Networking
 
                 switch ((PacketType)int.Parse(tmp_Client.PackageType))
                 {
+
                     case PacketType.PT_Hello:
-                    {
-                        string id = GenerateUUID();
-                        
-                        tmp_Client.id = id;                        
-                        tmp_Client.socket_client = remote;
-
-                        if (!paddle)
-                        {
-                            tmp_Client.PaddleInUse = 1.ToString();
-                            paddle = true;
-                        }
-                        else
-                            tmp_Client.PaddleInUse = 2.ToString();
-
-                        Debug.LogError(tmp_Client.socket_client);
-                       
-                        clients.Add(id, tmp_Client);
-
-                        tmp_Client.PackageType = 1.ToString();
-
-                        packageDataSnd = new byte[1024];
-                        SerializeClient(tmp_Client);
-                        msgToshow = tmp_Client.id.ToString();
-                        SendMsg(tmp_Client);
-
-                    }
+                        HandShakePlayer(tmp_Client);
                         break;
-                    case PacketType.PT_Acknowledge:
-                        break;
+
                     case PacketType.PT_InputData:
-
-                       
-                        MovePaddles(tmp_Client);
-                        Debug.LogError(world_Replication.Paddle1Pos + "   "  + world_Replication.Paddle2Pos);
-
-                        world_Replication.PackageType = 4.ToString();
-
-                        packageDataSnd = new byte[1024];
-                        SerializeData(world_Replication);
-
-                        
-                        foreach (KeyValuePair<string, Client> player in clients)
-                        {
-                            if (tmp_Client.id == player.Key)
-                                tmp_Client.socket_client = player.Value.socket_client;
-
-                        }
-
-                        SendMsg(tmp_Client);
-
-
-                        //msgToshow = tmp_Client.PaddleMovement;
-                        //Debug.Log(tmp_Client.PaddleMovement);
+                        BroadcastWorldState(tmp_Client);
                         break;
+
                     case PacketType.PT_Disconnect:
+                        //A
                         break;
-                    default:
-                        break;
+
                 }
             }
             catch (Exception e)
             {
                 Debug.LogWarning(e.Message);
-              
             }
 
-            
-
-            //Debug.Log(Encoding.ASCII.GetString(packageDataRcv));
             Thread.Sleep(50);
         }
     }
 
     void SendMsg(Client c)
     {
-        Debug.LogError("ANTES");
-        if (c.socket_client == null)
-            Debug.LogError("PackageEmpty");
-        if (c.socket_client != null)
-            Debug.LogError("PackageSendFull");
-
         listener.SendTo(packageDataSnd, packageDataSnd.Length, SocketFlags.None, c.socket_client);        //Send data to client
-        Debug.LogError("DESPUES");
     }
 
     string GenerateUUID()
@@ -206,7 +188,6 @@ public class NetworkingServer : Networking
         serializer.Serialize(stream, wR);
 
         packageDataSnd = stream.ToArray();
-
     }
 
     public void SerializeClient(Client c)
@@ -217,7 +198,6 @@ public class NetworkingServer : Networking
         serializer.Serialize(stream, c);
 
         packageDataSnd = stream.ToArray();
-
     }
 
     //Deserialize data from XML file
@@ -238,30 +218,19 @@ public class NetworkingServer : Networking
 
     public void MovePaddles(Client c)
     {
-        Debug.LogError("ADIOS MUY BUENAS 0");
 
         if (int.Parse(c.PaddleInUse) == 1)
         {
-            Debug.LogError("ADIOS MUY BUENAS 1");
-
             float paddle_pos = float.Parse(world_Replication.Paddle1Pos);
-
-            Debug.LogError("ADIOS MUY BUENAS 2");
-
             paddle_pos = Mathf.Clamp(float.Parse(world_Replication.Paddle1Pos) + float.Parse(c.PaddleMovement) * speed * (DateTime.Now.Ticks / 10000000), -yBound, yBound);
-
-            Debug.LogError("ADIOS MUY BUENAS 3");
-
             world_Replication.Paddle1Pos = paddle_pos.ToString();
         }
         else
         {
             float paddle_pos = float.Parse(world_Replication.Paddle2Pos);
             paddle_pos = Mathf.Clamp(float.Parse(world_Replication.Paddle2Pos) + float.Parse(c.PaddleMovement) * speed * (DateTime.Now.Ticks/10000000), -yBound, yBound);
-            
             world_Replication.Paddle2Pos = paddle_pos.ToString();
         }
-
     }
 
     //Initial launch of the ball
@@ -270,7 +239,6 @@ public class NetworkingServer : Networking
         Vel.x = Random.Range(0, 2) == 0 ? 1 : -1;
         Vel.y = Random.Range(0, 2) == 0 ? 1 : -1;
         ballRb.velocity = new Vector2(Vel.x, Vel.y) * initialVelocity;
-
     }
 
     #region InitializeAll 
